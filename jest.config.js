e8437e4e349b18d2dc125f0a9fc7024b7586589d
João Victor <joao15victor08@gmail.com>
2023-12-05 12:48:22 -0300

    Corrige build

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+"use strict";
+module.exports = {
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!<rootDir>/src/index.ts',
+        '!<rootDir>/src/*.spec.ts'
+    ],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'babel',
+    testMatch: ['**/*.spec.ts'],
+    roots: ['<rootDir>/src'],
+    transform: {
+        '\\.ts$': 'ts-jest'
+    },
+    clearMocks: true,
+    reporters: [
+        'default',
+        [
+            'jest-sonar',
+            {
+                outputDirectory: 'report',
+                outputName: 'sonar-report.xml'
+            }
+        ]
+    ]
+};
